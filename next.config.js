9a7cad0ae2b85162d3438a096476ce9d08aba424
CarmeloUrena <30450400+CarmeloUrena@users.noreply.github.com>
2026-03-26 12:03:07 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // Disable in dev to avoid aggressive caching
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withPWA(nextConfig);
